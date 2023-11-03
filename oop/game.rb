e78b9c0a3c668a9f480343f7b0f4f4cedeaa9012
Leonardo Prado <leonardoprado@gmail.com>
2023-11-03 13:39:05 -0300

    Implement score calculation

--- oop/game.rb
+++ oop/game.rb
@@ -34,6 +34,7 @@ class Game
     {
       total_kills: self.kills.count,
       players: self.players.names,
+      score: self.kills.score.transform_keys(&:name),
     }
   end
 
--- oop/kill_collection.rb
+++ oop/kill_collection.rb
@@ -20,4 +20,18 @@ class KillCollection
   def add(killer, victim, mod)
     @list << Kill.new(killer, victim, mod)
   end
+
+  def score
+    system_kills, player_kills = @list.partition { _1.killer.system? }
+
+    system_kills = system_kills.group_by(&:victim).map{ |k, v| [k, v.count] }.to_h
+
+    player_kills
+      .group_by(&:victim)
+      .map{ |k, v| [k, v.count] }
+      .sort_by(&:last)
+      .reverse
+      .to_h
+      .merge(system_kills) { |player, player_kills, system_kills| player_kills - system_kills }
+  end
 end
